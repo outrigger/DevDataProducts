63290b8f7b3badc054d378abd606ec1c963ac8db
Alex Sharma <alex_dta88@hotmail.com>
2015-09-13 18:21:52 +0800

    Edited minor typo in ui.R file

--- ui.R
+++ ui.R
@@ -9,7 +9,7 @@ shinyUI(fluidPage(
       img(src="titanic_sinking.png", style="display: block; margin-left: auto; margin-right: auto;"),
       h5("The sinking of the RMS Titanic is one of the most infamous shipwrecks in history.  On April 15, 1912, during her maiden voyage, the Titanic sank after colliding with an iceberg, killing 1502 out of 2224 passengers and crew. This sensational tragedy shocked the international community and led to better safety regulations for ships.", align = "justify"),
       h5("One of the reasons that the shipwreck led to such loss of life was that there were not enough lifeboats for the passengers and crew. Although there was some element of luck involved in surviving the sinking, some groups of people were more likely to survive than others, such as women, children, and the upper-class.", align = "justify"),
-      h5("This Shiny application analyses the types of people who are likely to survive through the use of machine learning techniques to predict which passengers survived the tragedy.", align = "justify"),
+      h5("This Shiny application analyses the types of people who are likely to survive through the use of machine learning techniques that predict which passengers survived the tragedy.", align = "justify"),
       br(),
       h5(strong("Click on the tabs above to find out more!"), align = "center"),
       br(),
